fdeafc8431b7e764c93aa1d2901d629171043693
Mayank <workformayank0@gmail.com>
2025-05-10 16:27:11 +0530

    completed the first version of the app

---
+++ start/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import logo from '../assets/investment-calculator-logo.png';
+export default function () {
+    return(
+        <header id="header">
+            <img src = {logo} alt="Logo showing a money bag" />
+            <h1>Investment Calculator</h1>
+        </header>
+    )
+}
